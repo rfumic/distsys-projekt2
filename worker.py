3480443765022d960e7c2f55fad9f2d0bf33fa01
rfumic <rokofumic@gmail.com>
2023-02-05 13:57:37 +0100

    Init and basic worker

---
+++ worker.py
@@ -0,0 +1,24 @@
+import aiohttp
+import asyncio
+import random
+import time
+import re
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+def number_of_words(string):
+    return len(re.findall(r'\w+',string))
+
+@routes.post('/worker')
+async def worker(request):
+    time.sleep(random.uniform(0.1,0.3))
+    req = await request.json()
+    wordsInFile = number_of_words(req['file'])
+    time.sleep(random.uniform(0.1,0.3))
+    
+    return web.json_response({'fileName': req['fileName'],'numberOfWords':wordsInFile},status=200)
+
+app = web.Application()
+app.router.add_routes(routes)
+web.run_app(app, port=8082)
